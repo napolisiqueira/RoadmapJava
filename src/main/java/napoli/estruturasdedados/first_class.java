1f659ae441cd68ed08d11e7388f0d57edd6cf4b2
Felipe Napoli Siquiera <imfelipenapoli@gmail.com>
2025-05-30 15:45:51 -0300

    more study

---
+++ src/main/java/napoli/estruturasdedados/first_class.java
@@ -0,0 +1,15 @@
+package napoli.estruturasdedados;
+
+public class first_class {
+
+    public static void main(String[] args) {
+        int A = 1;
+        int B = A;
+
+        System.out.println(A + "   " + B);
+        A = 4;
+        System.out.println(A + "   " + B);
+
+
+    }
+}
